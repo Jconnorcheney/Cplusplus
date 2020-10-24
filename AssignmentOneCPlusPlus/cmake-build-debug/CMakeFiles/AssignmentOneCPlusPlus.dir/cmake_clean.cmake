542766436f68aa5660399c7eb58ff11a913e1ccd
Jconnorcheney <54986451+Jconnorcheney@users.noreply.github.com>
2020-10-24 16:49:48 -0400

    Add files via upload
    Almost done

---
+++ AssignmentOneCPlusPlus/cmake-build-debug/CMakeFiles/AssignmentOneCPlusPlus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AssignmentOneCPlusPlus.exe"
+  "AssignmentOneCPlusPlus.exe.manifest"
+  "AssignmentOneCPlusPlus.pdb"
+  "CMakeFiles/AssignmentOneCPlusPlus.dir/main.cpp.obj"
+  "libAssignmentOneCPlusPlus.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AssignmentOneCPlusPlus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AssignmentOneCPlusPlus/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include <cstdlib>
+#include <regex>
+#include <string>
+using namespace std;
+//prototype
+void htmlConversionMethod(ifstream& inStream, ofstream& outStream);
+int main() {
+    ifstream inStream;
+    ofstream outStream;
+    string inputFileName;
+    string outputFileName;
+    /*https://stackoverflow.com/questions/11794144/
+     * regular-expression-for-valid-filename
+     * regex for proper windows filename, 10 upvote comment.*/
+    regex fileNameRegex("^[a-zA-Z0-9](?:[a-zA-Z0-9 ._-]*[a-zA-Z0-9])?$");
+
+    cout << "CPP to HTML file converter.\n\n";
+    cout << "Enter the name of the .cpp file you wish to convert.\n"
+            "Please use the file name without the extension:" << endl;
+    cin >> inputFileName;
+
+    do{
+        if(!regex_match(inputFileName,fileNameRegex)) {
+            cout << "Invalid Windows filename, try again:" << endl;
+           cin >> inputFileName;
+        }
+    }while(!regex_match(inputFileName,fileNameRegex));
+    /*variables for .ccp and .html files*/
+    outputFileName = inputFileName + ".html";
+    inputFileName = inputFileName + ".cpp";
+
+
+    try{
+        inStream.open(inputFileName.c_str());
+        if(inStream.fail()){
+            throw invalid_argument("Failed to open specified cpp file.");
+        }
+    }catch(const invalid_argument& e){
+        cout << "Failed to open specified cpp file." << endl;
+        exit(-1);
+    }
+
+
+    try{
+        outStream.open(outputFileName.c_str());
+        if(outStream.fail()){
+            throw invalid_argument("Failed to make specified html file.");
+        }
+    }catch(const invalid_argument& e){
+        cout << "Failed to make specified html file." << endl;
+    }
+    /*Alterations to the file done here, adding tags and calling method
+     * to change the '<' and '>'*/
+    outStream << "<PRE>" << endl;
+    htmlConversionMethod(inStream,outStream);
+    outStream << "</PRE>" << endl;
+    /*Originally had it changing the file from cpp to html, much easier to
+     * demonstrate just making a new file.*/
+//    rename(inputFileName.c_str(),outputFileName.c_str());
+
+    inStream.close();
+    outStream.close();
+
+    cout << outputFileName + " Created and Altered.";
+    return 0;
+}
+void htmlConversionMethod(ifstream& inStream, ofstream& outStream)
+{
+    char currentChar;
+
+    inStream.get(currentChar);
+
+    while (!inStream.eof())
+    {
+        if (currentChar == '<'){
+            outStream << "&lt;";
+        }
+        else if (currentChar == '>') {
+            outStream << "&gt;";
+        }
+        else {
+            outStream << currentChar;
+        }
+        inStream.get(currentChar);
+    }
+
+}
